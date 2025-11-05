a49277c69f5c9ab32a1a461d7294617bce5355c3
coretteket <33397397+Coretteket@users.noreply.github.com>
2025-11-05 10:42:47 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import '@fontsource-variable/work-sans';
+	import '../app.css';
+
+	import favicon from '$lib/assets/favicon.svg';
+	import logo from '$lib/assets/logo.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="text-stone-800">
+	<div class="border-b border-stone-300">
+		<header class="mx-auto flex max-w-7xl items-center justify-between px-4 pt-8 pb-7 text-lg">
+			<a href="/">
+				<img
+					src={logo}
+					width={150}
+					height={50}
+					alt="Open Publiceren"
+					fetchpriority="high"
+					loading="eager"
+					decoding="sync"
+				/>
+			</a>
+			<nav class="flex items-center gap-16">
+				<a href="/" class="text-stone-800 transition-colors hover:underline"> bestandsformaten </a>
+				<a href="/" class="text-stone-800 transition-colors hover:underline"> beleidskaders </a>
+				<a href="/" class="text-stone-800 transition-colors hover:underline"> over ons </a>
+				<a
+					href="/"
+					class="rounded-lg bg-amber-300 px-5 py-2 font-medium text-amber-950 transition-colors hover:underline"
+				>
+					keuzehulp
+				</a>
+			</nav>
+		</header>
+	</div>
+	<main class="mx-auto max-w-7xl p-4">
+		{@render children?.()}
+	</main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import hero from '$lib/assets/hero.svg';
+</script>
+
+<header class="my-12 flex justify-between gap-20">
+	<div>
+		<h1 class="mt-2 mb-8 text-5xl font-medium text-stone-950">Open Publiceren</h1>
+		<p class="mb-8 text-xl">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+			eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		</p>
+		<div class="flex items-center gap-4">
+			<a
+				href="/"
+				class="rounded-lg bg-amber-300 px-5 py-3 text-xl font-medium text-amber-950 transition-colors hover:underline"
+			>
+				start keuzehulp
+			</a>
+			<a
+				href="/"
+				class="rounded-lg border border-stone-300 px-5 py-3 text-xl font-medium text-stone-600 transition-colors hover:underline"
+			>
+				lees meer
+			</a>
+		</div>
+	</div>
+	<img src={hero} width={500} alt="" fetchpriority="high" loading="eager" decoding="sync" />
+</header>
