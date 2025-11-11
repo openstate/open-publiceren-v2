3bf2aef394d5387168b7baf91974543557c4a517
coretteket <33397397+Coretteket@users.noreply.github.com>
2025-11-11 14:09:23 +0100

    fix: don't externalize cva For some reason, Vite tries to `import 'cva';` in the build output unless explicitly told not to externalize as is done here. Excluding this line does not cause build errors, but it does cause HTTP 500 errors when deployed.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	ssr: { noExternal: ["cva"] } // necessary to avoid HTTP 500 errors on deployment
 });
